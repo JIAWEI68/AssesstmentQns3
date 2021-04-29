291285a6e57f5cc488187353dd520651ce18673a
JIAWEI68 <ang.jiawei56@gmail.com>
2021-04-29 16:15:47 +0800

    Assestment Qns 3

---
+++ AssestmentQns3/src/main/java/com/example/assestmentqns3/AssignmentQ3.java
@@ -0,0 +1,15 @@
+package com.example.assestmentqns3;
+
+public class AssignmentQ3 {
+    public static void main(String[] args) {
+        int[] array = new int[8];
+        int[] friends = {55,24,65,41,13,22,76,90};
+        for (int num=0;num<array.length;num++){
+            if (friends[num]>=55){
+                System.out.println(friends[num]);
+            }
+        }
+
+
+    }
+}
---
+++ AssestmentQns3/src/main/java/com/example/assestmentqns3/MyClass.java
@@ -0,0 +1,4 @@
+package com.example.assestmentqns3;
+
+public class MyClass {
+}
